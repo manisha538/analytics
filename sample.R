fcc3f66d1c9509f0b6e91cfc3e0a0ef1c3755b86
manisha538 <manisha.ghatuary@gmail.com>
2019-09-29 13:04:10 +0530

    sbwjd

---
+++ sample.R
@@ -0,0 +1,26 @@
+#sample
+
+set.seed(1234)
+sample(1:10)
+
+#
+LETTERS[1:26]
+sample(LETTERS[1:26])
+
+?sample
+sample(x=LETTERS[1:26], size=100, replace=T)
+
+gender = c('M','F')
+gender
+sample(x=gender)
+sample(x=gender, size=100) #error
+sample(x=gender, size=100, replace=T)
+x=sample(x=gender, size=100, replace=T)
+x
+table(x)
+x2=sample(x=gender,size=10000, replace=T, prob=c(.4,.6))
+table(x2)
+course=c('MBA','BBA')
+course
+y=sample(x=course, size=1000, replace=T, prob=c(0.6,0.4))
+table(y)
